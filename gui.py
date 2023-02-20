525b3237724580257eb3b46541102d9a5813e34e
ClinCliu <edymclinciu@gmail.com>
2023-02-20 23:33:05 +0000

    Add files via upload

--- gui.py
+++ gui.py
@@ -1,30 +1,33 @@
-import tkinter as tk
-import better_than_clincliu_calculator as calc
+from tkinter import *
+from calc import *
 
 def makeText(str):
-    text = tk.Label(text=str)
+    text = Label(window, text=str)
     return text
 
 def getEquation(event):
     equation = equation_input.get()
-    equation = calc.format_equation(equation)
-    calc.simplify(equation)
-    equation_input.delete(0, tk.END)
+    equation = format_equation(equation)
+    simplify(equation)
+    equation_input.delete(0, END)
     equation_input.insert(0, equation)
 
-window = tk.Tk()
+window = Tk()
+window.title("Calculator")
+window.resizable(False, False)
 
 makeText("Calculator").pack()
 makeText("    ").pack()
 makeText("Enter in a equation, no spaces").pack()
 
-equation_input = tk.Entry(width=50)
+equation_input = Entry(window, width=50)
 equation_input.pack()
 
-solve = tk.Button(height=25, width=50, text="Solve")
+makeText(" ").pack()
+
+solve = Button(window, height=10, width=45, text="Solve")
 solve.pack()
 
 equation = solve.bind("<Button-1>", getEquation)
-
 
 window.mainloop()
